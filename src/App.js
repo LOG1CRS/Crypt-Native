213decbbc130234701b8a4c9bbde227518a5ce4e
André Marco Ruiz Sandoval <log1c.rs.psc@gmail.com>
2020-11-30 20:04:11 -0600

    Bottom navigation added with icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,63 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import colors from './assets/style/colors';
+
 import MainStack from './navigation/MainStack';
+import SearchStack from './navigation/SearchStack';
+import FavoritesStack from './navigation/FavoritesStack';
+
+const Tabs = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <MainStack />
+      <Tabs.Navigator
+        tabBarOptions={{
+          inactiveTintColor: '#fefefe',
+          style: {
+            shadowColor: colors.blackPearl,
+            borderTopWidth: 0,
+            backgroundColor: colors.blackPearl,
+            height: 60,
+          },
+          labelStyle: {
+            marginBottom: 7,
+          },
+          iconStyle: {
+            marginTop: 4,
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="Feed"
+          component={MainStack}
+          options={{
+            tabBarIcon: ({ size, color }) => {
+              return <Ionicons name="ios-cash" size={size} color={color} />;
+            },
+          }}
+        />
+        <Tabs.Screen
+          name="Search"
+          component={SearchStack}
+          options={{
+            tabBarIcon: ({ size, color }) => {
+              return <Ionicons name="ios-search" size={size} color={color} />;
+            },
+          }}
+        />
+        <Tabs.Screen
+          name="Favorites"
+          component={FavoritesStack}
+          options={{
+            tabBarIcon: ({ size, color }) => {
+              return <Ionicons name="ios-heart" size={size} color={color} />;
+            },
+          }}
+        />
+      </Tabs.Navigator>
     </NavigationContainer>
   );
 };
---
+++ src/navigation/SearchStack.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { TextInput } from 'react-native';
+import colors from '../assets/style/colors';
+
+import SearchScreen from '../screens/SearchScreen';
+
+const Stack = createStackNavigator();
+
+const SearchStack = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: colors.blackPearl,
+          shadowColor: colors.blackPearl,
+        },
+        headerTintColor: colors.white,
+      }}
+    >
+      <Stack.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{
+          title: 'Search',
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default SearchStack;
